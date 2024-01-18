10a91ee953d133d8997a0166ddf203ab43e7045d
lennymercadovaldez <71667982+lennymercadovaldez@users.noreply.github.com>
2024-01-18 17:25:52 -0500

    Remove Navbar component from App.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -8,7 +8,6 @@ function App() {
 
   return (
     <>
-    <Navbar />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
